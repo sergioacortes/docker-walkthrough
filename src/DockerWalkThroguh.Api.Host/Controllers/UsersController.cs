b39c84889306d0161e9173d26830e076c21ed54c
Sergio Antonio Cortes Ocampo <sergio.cortes>
2024-12-13 14:00:39 +0100

    Add docker configuration

---
+++ src/DockerWalkThroguh.Api.Host/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DockerWalkThroguh.Api.Host.Controllers;
+
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+
+    public UsersController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(_configuration.GetConnectionString("DefaultConnection"));
+    }
+    
+}
