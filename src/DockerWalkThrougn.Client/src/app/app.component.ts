f69c9982a1f0d56feeb1f073e6973f0e20d15c43
Sergio Antonio Cortes Ocampo <sergio.cortes>
2024-12-13 12:10:38 +0100

    Project creation

---
+++ src/DockerWalkThrougn.Client/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent implements OnInit  {
+  
+  data = [];
+  title = 'DockerWalkThrougn.Client';
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+  ngOnInit(): void {
+  }
+
+}
